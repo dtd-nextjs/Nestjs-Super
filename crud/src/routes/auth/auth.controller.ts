b0cc7ef85f79ae949bfcbdac7287f55ad1e3920f
Dư Thanh Được <duthanhduoc@gmail.com>
2025-02-04 21:09:46 +0700

    Serialization - Chuẩn hóa dữ liệu trả về

--- crud/src/routes/auth/auth.controller.ts
+++ crud/src/routes/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common'
-import { RegisterBodyDTO } from 'src/routes/auth/auth.dto'
+import { Body, Controller, Post, SerializeOptions } from '@nestjs/common'
+import { RegisterBodyDTO, RegisterResDTO } from 'src/routes/auth/auth.dto'
 import { AuthService } from 'src/routes/auth/auth.service'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @SerializeOptions({ type: RegisterResDTO })
   @Post('register')
   register(@Body() body: RegisterBodyDTO) {
-    console.log(body)
-    return 'register'
-    // return this.authService.register(body)
+    return this.authService.register(body)
   }
 }
