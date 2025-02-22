a22dec82cb31c007b7a38ba8aac168dddd835819
Dư Thanh Được <duthanhduoc@gmail.com>
2025-02-22 15:40:01 +0700

    Gửi email bằng template html

--- ecom/src/shared/services/email.service.ts
+++ ecom/src/shared/services/email.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from 'src/shared/config'
+import fs from 'fs'
+import path from 'path'
+const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), {
+  encoding: 'utf-8',
+})
 
 @Injectable()
 export class EmailService {
@@ -8,13 +13,13 @@ export class EmailService {
   constructor() {
     this.resend = new Resend(envConfig.RESEND_API_KEY)
   }
-
   sendOTP(payload: { email: string; code: string }) {
+    const subject = 'Mã OTP'
     return this.resend.emails.send({
-      from: 'Ecommerce <onboarding@resend.dev>',
+      from: 'Nest.js Ecommerce <no-reply@duoc.id.vn>',
       to: [payload.email],
-      subject: 'Mã OTP',
-      html: `<strong>${payload.code}</strong>`,
+      subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
