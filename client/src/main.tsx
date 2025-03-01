99dd50b7d73afb21b214db0dedf407d5ad5d8b00
Dư Thanh Được <duthanhduoc@gmail.com>
2025-03-01 12:24:53 +0700

    Source FrontEnd Vite React để test chức năng login với Google

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Layout from './Layout.tsx'
+import Login from './Login.tsx'
+import Home from './Home.tsx'
+import Oauth from './Oauth.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='oauth-google-callback' element={<Oauth />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
