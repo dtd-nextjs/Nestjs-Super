5bf8ca83eabaa9758b65b2f6bda368222bb02148
Dư Thanh Được <duthanhduoc@gmail.com>
2025-04-14 22:26:38 +0700

    Filter Product

--- ecom/src/routes/product/product.service.ts
+++ ecom/src/routes/product/product.service.ts
@@ -14,6 +14,12 @@ export class ProductService {
       limit: props.query.limit,
       languageId: I18nContext.current()?.lang as string,
       isPublic: true,
+      brandIds: props.query.brandIds,
+      minPrice: props.query.minPrice,
+      maxPrice: props.query.maxPrice,
+      categories: props.query.categories,
+      name: props.query.name,
+      createdById: props.query.createdById,
     })
     return data
   }
