8da0a3e2f00927355655ae7c57d0d98878b4dce2
Dư Thanh Được <duthanhduoc@gmail.com>
2025-04-26 01:32:47 +0700

    Rollback update khi queue bị lỗi

--- ecom/src/routes/order/order.service.ts
+++ ecom/src/routes/order/order.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common'
-import { CreateOrderBodyType, GetOrderListQueryType, GetOrderListResType } from 'src/routes/order/order.model'
-import { OrderProducer } from 'src/routes/order/order.producer'
+import { CreateOrderBodyType, GetOrderListQueryType } from 'src/routes/order/order.model'
 import { OrderRepo } from 'src/routes/order/order.repo'
 
 @Injectable()
 export class OrderService {
-  constructor(
-    private readonly orderRepo: OrderRepo,
-    private orderProducer: OrderProducer,
-  ) {}
+  constructor(private readonly orderRepo: OrderRepo) {}
 
   async list(userId: number, query: GetOrderListQueryType) {
     return this.orderRepo.list(userId, query)
@@ -16,10 +12,7 @@ export class OrderService {
 
   async create(userId: number, body: CreateOrderBodyType) {
     const result = await this.orderRepo.create(userId, body)
-    await this.orderProducer.addCancelPaymentJob(result.paymentId)
-    return {
-      data: result.orders,
-    }
+    return result
   }
 
   cancel(userId: number, orderId: number) {
